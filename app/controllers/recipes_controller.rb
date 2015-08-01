86e18902a1af7624ace756573424b00cc2b1e722
Mwiti Gitonga <mwiti.k.gitonga@gmail.com>
2015-08-01 08:51:47 +0300

    Creating partials for the new edit and index erb

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,52 @@
+class RecipesController < ApplicationController
+
+def index
+@recipes = Recipe.all
+end
+
+def show
+ @recipe = Recipe.find(params[:id])
+end
+
+def new
+@recipe = Recipe.new
+end
+
+def create
+@recipe = Recipe.new(recipe_params)
+
+@recipe.chef = Chef.find(2)
+
+if @recipe.save
+	#do something
+flash[:success] = "Your recipe was created succesfully!"
+redirect_to recipes_path
+else
+
+	render :new
+end
+end
+def edit
+@recipe = Recipe.find(params[:id])
+end
+def update
+@recipe = Recipe.find(params[:id])
+if @recipe.update(recipe_params)
+
+	flash[:success] = "Your recipe was updated succefully! "
+	redirect_to recipes_path(@recipe)
+
+else
+
+	render :edit
+
+end
+end
+private
+
+def recipe_params
+
+	params.require(:recipe).permit(:name, :summary, :description)
+end
+
+end
